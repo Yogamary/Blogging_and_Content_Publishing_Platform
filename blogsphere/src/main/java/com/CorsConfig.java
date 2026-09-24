428b4b54a856062cab0cb808ed7e0db9db11d534
Yogamary_M <yogamary2006@gmail.com>
2026-09-24 21:18:34 +0530

    Allow deployed frontend in CORS

--- blogsphere/src/main/java/com/CorsConfig.java
+++ blogsphere/src/main/java/com/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
         configuration.setAllowedOrigins(
                 List.of(
                     "http://localhost:5500",
-                    "http://127.0.0.1:5500"
+                    "http://127.0.0.1:5500",
+                    "https://blogsphere-frontend-8lns.onrender.com"
                 )
         );
 
